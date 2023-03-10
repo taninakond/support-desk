b7620aafca8826cc7356bca4f1993f54bad7a011
Tanin Ahmed <taninakond@gmail.com>
2023-03-10 22:43:09 +0600

    Add routes and controllrs

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({message: 'Welcome to Support Desk API'}));
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));

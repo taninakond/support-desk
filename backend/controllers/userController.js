6494825b27e36fd0e0c43acc340c2d8b5253df80
Tanin Ahmed <taninakond@gmail.com>
2023-03-11 22:41:51 +0600

    Add User Model and install jwt and setup Register user and login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModes');
 
 // @desc    Register a new user
 // @route   /api/users
@@ -11,15 +14,69 @@ const registerUser = asyncHandler( async (req, res) => {
         res.status(400);
         throw new Error('please include all fields');
     }
-    res.send("Register Route");
+
+    // Find if user already exists
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    }else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 })
 
 // @desc    Login a user
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler( async (req, res) => {
-    res.send("Login Route");
-})
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email});
+
+    // Check user and password match
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    }else {
+        res.status(401);
+        throw new Error('Invalid Credentials');
+    }
+});
+
+// Generate Token 
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+}
+
+
 
 module.exports = {
     registerUser,

dc76cb162f614f473c13a270a34e0791469b4e7d
Tanin Ahmed <taninakond@gmail.com>
2023-03-11 23:47:45 +0600

    Protect Routes and Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,6 +69,20 @@ const loginUser = asyncHandler( async (req, res) => {
     }
 });
 
+
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler( async (req, res) => {
+
+    const user = {
+        id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+    res.status(200).json(user);
+})
+
 // Generate Token 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -80,5 +94,6 @@ const generateToken = (id) => {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
